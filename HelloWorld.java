cb4b3845d311b66d8d2803912c637b8ce24e5e6a
21wh1a6656 <96616819+21wh1a6656@users.noreply.github.com>
2024-07-31 11:24:24 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,6 @@ class HelloWorld {
     public static void main(String args[])
     {
         System.out.println("Hello, World");
-        System.out.println("Java automatic build");
+        System.out.println("Java application automatic build");
     }
 }
